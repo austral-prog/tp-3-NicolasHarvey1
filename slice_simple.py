1cc177741fa0aaad0c529846508052f7b3324b69
NicolasHarvey1 <162722159+NicolasHarvey1@users.noreply.github.com>
2024-04-05 12:23:19 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,10 +1,9 @@
 def slice_simple():
-    texto = "Awesome"
-    mitad_texto = int(len(texto)/2)
-    print(texto[0:3].lower())
-    print((texto[mitad_texto-1]+texto[mitad_texto]+texto[mitad_texto+1]).lower())
-    print(texto[0:4].lower()+texto[len(texto)-3:len(texto)].lower())
+	texto = "Awesome"
+	mitad_texto = int(len(texto)/2)
 
-
+	print(texto[0:3].lower())
+	print((texto[mitad_texto-1]+texto[mitad_texto]+texto[mitad_texto+1]).lower())
+	print((texto[0:4]+texto[len(texto)-3:len(texto)]).lower())
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
