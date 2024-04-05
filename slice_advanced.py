11be55a872e1e4d419793396bd8fdecb4b6fb4fe
NicolasHarvey1 <162722159+NicolasHarvey1@users.noreply.github.com>
2024-04-05 12:37:33 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -2,6 +2,7 @@ def slice_advanced():
     # Código a implementar utilizando input.
     texto = input("escribir texto: ")
     print(texto[4:len(texto):2])
+    
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
